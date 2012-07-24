c5980dfcb513bc4a2f65028d3e34ea65e9ce9eda
Wang keke <cokecoffe@gmail.com>
2012-07-25 00:21:51 +0800

    拷贝的理解

--- copy/copy/main.m
+++ copy/copy/main.m
@@ -13,24 +13,67 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+#if 0
+        //不可变对象的copy
+        NSString *str = [NSString stringWithFormat:@"123"];
+                
+        NSString *cpstr = [str copy];//浅拷贝，str:2 ,cpstr:2
+       
+        NSLog(@"STR:%p recount = %ld",str,[str retainCount]);
+        NSLog(@"CPstr:%p recount = %ld",cpstr,[cpstr retainCount]);
+#endif
         
-        NSString *str = [[NSString alloc]initWithFormat:@"123"];//[NSString stringWithFormat:@"123"];
+#if 0
+        //不可变对象的mutableCopy
+        NSString *str = [NSString stringWithFormat:@"123"];
         
-        NSLog(@"str:%p recount = %ld",str,[str retainCount]);
+        NSMutableString *cpstr = [str mutableCopy];//深拷贝，str:1 ,cpstr:1
         
-        NSString *cpstr = [str copy];
+        NSLog(@"STR:%p recount = %ld",str,[str retainCount]);
+        NSLog(@"CPstr:%p recount = %ld",cpstr,[cpstr retainCount]);
+#endif
+        
+     
+#if 0
+        //可变对象的copy
+        NSString *str = [NSMutableString stringWithFormat:@"123"];
+        
+        NSString *cpstr = [str copy];//深拷贝，str:1 ,cpstr:1
+        
+        NSLog(@"STR:%p recount = %ld",str,[str retainCount]);
+        NSLog(@"CPstr:%p recount = %ld",cpstr,[cpstr retainCount]);
+#endif        
+
+#if 1
+        //可变对象的mutableCopy
+        NSString *str = [NSMutableString stringWithFormat:@"123"];
+        
+        NSMutableString *cpstr = [str mutableCopy];//深拷贝，str:1 ,cpstr:1
+        
+        NSLog(@"STR:%p recount = %ld",str,[str retainCount]);
+        NSLog(@"CPstr:%p recount = %ld",cpstr,[cpstr retainCount]);
+#endif
+        
+#if 0
+        NSArray *array = [NSArray arrayWithObjects:@"a",@"b",@"c",nil];
+        
+        NSArray *cparr = [array copy];
+        
+        NSLog(@"array:%p recount = %ld",array,[array retainCount]);
+        NSLog(@"cpArray:%p recount = %ld",cparr,[cparr retainCount]);
+#endif
+        
+#if 0
+        NSArray *array = [NSArray arrayWithObjects:@"a",@"b",@"c",nil];
+        
+        NSArray *cparr = [array mutableCopy];
+        
+        NSLog(@"array:%p recount = %ld object0:%p",array,[array retainCount],[array objectAtIndex:0]);
+        NSLog(@"cpArray:%p recount = %ld object1:%p",cparr,[cparr retainCount],[cparr objectAtIndex:0]);
+#endif
         
         
-        NSLog(@"cpstr:%p recount = %ld",cpstr,[cpstr retainCount]);
-        
-        
-        [cpstr retain];
-        NSLog(@"str:%p recount = %ld",str,[str retainCount]);
-         NSLog(@"cpstr:%p recount = %ld",cpstr,[cpstr retainCount]);
-        
-        
+                
     }
     return 0;
 }
