7dd0db5fae10af5b66057d00725f07c9fb34e993
Wang keke <cokecoffe@gmail.com>
2012-08-26 23:24:33 +0800

    XMPP Demo
    XMPP的例子，实现了显示好友列表，基本聊天功能。

---
+++ XMPP_Demo/main.m
@@ -0,0 +1,20 @@
+
+
+//
+//  main.m
+//  XMPP_Demo
+//
+//  Created by wangkeke on 12-8-19.
+//  Copyright (c) 2012年 wangkeke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
