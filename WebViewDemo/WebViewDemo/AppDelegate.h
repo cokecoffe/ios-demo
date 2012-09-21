67328e0ac08501d7df95a1dbea835dd873fe8c29
Wangkeke <cokecoffe@gmail.com>
2012-09-21 16:14:05 +0800

    WebView Demo
    WebView <-----> IOS

---
+++ WebViewDemo/WebViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WebViewDemo
+//
+//  Created by keke Wang on 12-9-21.
+//  Copyright (c) 2012年 checkauto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WebViewDemo/WebViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebViewDemo
+//
+//  Created by keke Wang on 12-9-21.
+//  Copyright (c) 2012年 checkauto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
