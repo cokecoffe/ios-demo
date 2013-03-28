32694181fca9b434e8d8518dbf63118994102786
wangkeke <cokecoffe@gmail.com>
2013-03-28 11:46:16 +0800

    mgbox demo

---
+++ MGBox2-master/Demo App/Demo App/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  MGBox2 Demo App
+//
+//  Created by Matt Greenfield on 25/09/12.
+//  Copyright (c) 2012 Big Paua. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end
---
+++ MGBox2-master/MGBox/MGTableBoxStyled.h
@@ -0,0 +1,10 @@
+//
+//  Created by Matt Greenfield on 24/05/12
+//  http://bigpaua.com/
+//
+
+#import "MGTableBox.h"
+
+@interface MGTableBoxStyled : MGTableBox
+
+@end
