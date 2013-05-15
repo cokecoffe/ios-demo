5cf1d0e445511bbe6582ea19012e4728254b99ba
Wangkeke <cokecoffe@gmail.com>
2013-05-15 19:55:30 +0800

    mv folder

---
+++ WSDL2ObjC_example/WSDL2ObjC/UXViewController.h
@@ -0,0 +1,13 @@
+//
+//  UXViewController.h
+//  WSDL2ObjC
+//
+//  Created by wang keke on 13-3-27.
+//  Copyright (c) 2013年 uxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UXViewController : UIViewController
+
+@end
